4d1e46f21baf52e21ec4ea7b382d1f468897cf42
alan.wan <alan.wan@cn.accurascience.com>
2016-02-25 15:57:37 +0800

    commit the projetct initial.

---
+++ mysite/polls/admin.py
@@ -0,0 +1,48 @@
+from django.contrib import admin
+from .models import Question,Choice
+
+#class QuestionAdmin(admin.ModelAdmin):
+#    fields = ['pub_date','question_text']
+#admin.site.register(Question,QuestionAdmin)
+
+#class QuestionAdmin(admin.ModelAdmin):
+#    fieldsets = [
+#            ("Text information",{'fields':['question_text']}),
+#            ("Date information",{'fields':['pub_date']}),
+#            ]
+#admin.site.register(Question,QuestionAdmin)
+
+#class QuestionAdmin(admin.ModelAdmin):
+#    fieldsets = [
+#            ("Text information",{'fields':['question_text']}),
+#            ("Date information",{'fields':['pub_date'],'classes':['collapse']}),
+#            ]
+#admin.site.register(Question,QuestionAdmin)
+#admin.site.register(Choice)
+
+#class ChoiceInline(admin.StackedInline):
+#    model = Choice
+#    extra = 3
+#
+#class QuestionAdmin(admin.ModelAdmin):
+#    fieldsets = [
+#            ("Text information",{'fields':['question_text']}),
+#            ("Date information",{'fields':['pub_date'],'classes':['collapse']})]
+#    inlines = [ChoiceInline]
+#
+#admin.site.register(Question,QuestionAdmin)
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+            ("Text information",{'fields':['question_text']}),
+            ("Date information",{'fields':['pub_date'],'classes':['collapse']})]
+    list_display = ('question_text','pub_date','was_published_recently')
+    inlines = [ChoiceInline]
+    list_filter = ['pub_date']
+    search_fields=['question_text']
+
+admin.site.register(Question,QuestionAdmin)
